401ff1a50d358363e22084b109d9c2f2a0741b76
ChipolaThug <timothe.caillaud@gmail.com>
2022-05-11 21:56:13 +0200

    Ajout barre de recherche pour les jeux

--- src/components/Knowledge/BoxList.tsx
+++ src/components/Knowledge/BoxList.tsx
@@ -7,38 +7,60 @@ import {
     fetchVolunteerKnowledgeSetIfNeed,
     useVolunteerKnowledge,
 } from "../../store/volunteerKnowledgeSet"
+import { DetailedBox } from "../../services/boxes"
 
 const BoxList: React.FC = (): JSX.Element | null => {
     const detailedBoxes = useSelector(selectSortedUniqueDetailedBoxes)
     const [volunteerKnowledge, saveVolunteerKnowledge] = useVolunteerKnowledge()
     const [showUnknownOnly, setShowUnknownOnly] = useState(false)
+    const [searchGame, setSearchGame] = useState("")
 
     const onShowUnknownOnly = (e: React.ChangeEvent<HTMLInputElement>) =>
         setShowUnknownOnly(e.target.checked)
 
     if (!detailedBoxes || detailedBoxes.length === 0) return null
 
+    const isGameSearched = (box: any) => {
+        const lowerTitle = box.title.toLowerCase()
+        const lowerSearch = searchGame.toLowerCase()
+        return lowerTitle.includes(lowerSearch)
+    }
+
     const boxesToShow = detailedBoxes.filter(
         (box) =>
             !box ||
-            !showUnknownOnly ||
-            !volunteerKnowledge ||
-            (!volunteerKnowledge.ok.includes(box.gameId) &&
-                !volunteerKnowledge.bof.includes(box.gameId) &&
-                !volunteerKnowledge.niet.includes(box.gameId))
+            (isGameSearched(box) &&
+                (!showUnknownOnly ||
+                    !volunteerKnowledge ||
+                    (!volunteerKnowledge.ok.includes(box.gameId) &&
+                        !volunteerKnowledge.bof.includes(box.gameId) &&
+                        !volunteerKnowledge.niet.includes(box.gameId))))
     )
 
     return (
         <div>
-            <label className={styles.showUnknownOnlyLabel}>
-                <input
-                    type="checkbox"
-                    name="showUnknownOnly"
-                    onChange={onShowUnknownOnly}
-                    checked={showUnknownOnly}
-                />{" "}
-                Uniquement les non-renseignés
-            </label>
+            <div className={styles.filters}>
+                <label className={styles.showUnknownOnlyLabel}>
+                    <input
+                        type="checkbox"
+                        name="showUnknownOnly"
+                        onChange={onShowUnknownOnly}
+                        checked={showUnknownOnly}
+                    />{" "}
+                    Uniquement les non-renseignés
+                </label>
+                <label htmlFor="filterInput">
+                    <span className={styles.searchLabel}>Rechercher un jeu...</span>
+                    <input
+                        placeholder="Rechercher un jeu..."
+                        type="text"
+                        id="filterInput"
+                        name="filterInput"
+                        value={searchGame}
+                        onChange={(e) => setSearchGame(e.target.value)}
+                    />
+                </label>
+            </div>
             <ul className={styles.boxList}>
                 {boxesToShow.map((detailedBox: any) => (
                     <BoxItem
